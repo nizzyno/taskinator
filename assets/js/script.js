a4977134df418d0fe0823a8132af4f9f0af11c8b
Noah <mcbeasty11.nh@icloud.com>
2021-03-16 16:22:17 -0500

    dynamically created buttons and select

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
+// var declarations
 var formEl=document.querySelector("#save-task");
 var tasksToDoEl=document.querySelector("#tasks-to-do");
+var taskIdCounter=0;
 
 var taskFormHandler=function(event){
     event.preventDefault();
     var taskNameInput=document.querySelector("input[name='task-name']").value;
-    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    var taskTypeInput=document.querySelector("select[name='task-type']").value;
 
      // check if inputs are empty (validate)
      if (taskNameInput === "" || taskTypeInput === "") {
@@ -28,22 +30,72 @@ var taskFormHandler=function(event){
   createTaskEl(taskDataObj);
 };
 
-var createTaskEl = function(taskDataObj) {
+var createTaskEl=function(taskDataObj) {
     // create list item
-var listItemEl = document.createElement("li");
-listItemEl.className = "task-item";
+var listItemEl=document.createElement("li");
+listItemEl.className="task-item";
 
+  // add task id as a custom attribute
+  listItemEl.setAttribute("data-task-id", taskIdCounter);
 // create div to hold task info and add to list item
-var taskInfoEl = document.createElement("div");
+var taskInfoEl=document.createElement("div");
 // give it a class name
-taskInfoEl.className = "task-info";
+taskInfoEl.className="task-info";
 // add HTML content to div
-taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+taskInfoEl.innerHTML="<h3 class='task-name'>"+taskDataObj.name+"</h3><span class='task-type'>"+taskDataObj.type+"</span>";
 listItemEl.appendChild(taskInfoEl);
+
+var taskActionsEl=createTaskActions(taskIdCounter);
+listItemEl.appendChild(taskActionsEl);
 
 // add entire list item to list
 tasksToDoEl.appendChild(listItemEl);
+
+ // increase task counter for next unique id
+ taskIdCounter++;
+//  console.log(taskIdCounter); or console.dir(window.document) then go to "elements"
 };
+
+var createTaskActions=function(taskId){
+    var actionContainerEl=document.createElement("div");
+    actionContainerEl.className="task-actions";
+
+    // create edit button
+    var editButtonEl=document.createElement("button");
+    editButtonEl.textContent="Edit";
+    editButtonEl.className="btn edit-btn";
+    editButtonEl.setAttribute("data-task-id", taskId);
+
+    actionContainerEl.appendChild(editButtonEl);
+
+    // create delete button
+    var deleteButtonEl=document.createElement("button");
+    deleteButtonEl.textContent="Delete";
+    deleteButtonEl.className="btn delete-btn";
+    deleteButtonEl.setAttribute("data-task-id", taskId);
+
+    actionContainerEl.appendChild(deleteButtonEl);
+
+    var statusSelectEl=document.createElement("select");
+    statusSelectEl.className="select-status";
+    statusSelectEl.setAttribute("name", "status-change");
+    statusSelectEl.setAttribute("data-task-id", taskId);
+
+     actionContainerEl.appendChild(statusSelectEl);
+
+     var statusChoices=["To Do", "In Progress", "Completed"];
+     for(var i=0;i<statusChoices.length; i++){
+         //create option element
+         var statusOptionEl=document.createElement("option");
+         statusOptionEl.textContent=statusChoices[i];
+         statusOptionEl.setAttribute("value", statusChoices[i]);
+
+         //append to select
+         statusSelectEl.appendChild(statusOptionEl);
+     }
+
+    return actionContainerEl;
+}
 
 formEl.addEventListener("click", taskFormHandler); // "submit" instead of "click" for enter key to work but it's breaking code now.
 
